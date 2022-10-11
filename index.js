faa501dc4ef7f1afc8fda480fd98b5f35c231d5b
HARSH DUDHAT <106002779+HARSHDUDHAT07@users.noreply.github.com>
2022-10-11 08:44:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,63 @@ const newsdetails = document.getElementById("newsdetails");
 
 // Array
 var newsDataArr = [];
+function displayNews() {
+  newsdetails.innerHTML = "";
+
+  // if(newsDataArr.length == 0) {
+  //     newsdetails.innerHTML = "<h5>No data found.</h5>"
+  //     return;
+  // }
+
+  newsDataArr.forEach((news)=>{
+    var date = news.publishedAt.split("T");
+
+    var col = document.createElement("div");
+    col.className = "col-sm-12 col-md-4 col-lg-3 m-2 p-2 card card-body";
+  
+    var card = document.createElement("div");
+    card.className = "p-2";
+
+    var image = document.createElement("img");
+    image.setAttribute("height", "matchparent");
+    image.setAttribute("width", "100%");
+    image.src =
+      news.urlToImage ||
+      "https://i-invdn-com.investing.com/news/world_news_2_69x52._800x533_L_1419494365.jpg";
+
+    var cardBody = document.createElement("div");
+
+    var newsHeading = document.createElement("h5");
+    newsHeading.className = "card-title";
+    newsHeading.innerHTML = news.title;
+
+    var dateHeading = document.createElement("h6");
+    dateHeading.className = "text-primary";
+    dateHeading.innerHTML = date[0];
+
+    var discription = document.createElement("p");
+    discription.className = "text-muted";
+    discription.innerHTML = news.description;
+
+    var link = document.createElement("a");
+    link.className = "btn btn-dark";
+    link.setAttribute("target", "_blank");
+    link.href = news.url;
+    link.innerHTML = "Read more";
+
+    cardBody.appendChild(newsHeading);
+    cardBody.appendChild(dateHeading);
+    cardBody.appendChild(discription);
+    cardBody.appendChild(link);
+
+    card.appendChild(image);
+    card.appendChild(cardBody);
+
+    col.appendChild(card);
+
+    newsdetails.appendChild(col);
+  });
+}
 
 // apis
 // const API_KEY = "9411efe63ff6427ca1ca36cc6a6adfdc";//neel goyani
@@ -305,60 +362,3 @@ const fetchWeatherNews = async () =>{
 //   displayNews();
 // };
 
-function displayNews() {
-  newsdetails.innerHTML = "";
-
-  // if(newsDataArr.length == 0) {
-  //     newsdetails.innerHTML = "<h5>No data found.</h5>"
-  //     return;
-  // }
-
-  newsDataArr.forEach((news)=>{
-    var date = news.publishedAt.split("T");
-
-    var col = document.createElement("div");
-    col.className = "col-sm-12 col-md-4 col-lg-3 m-2 p-2 card card-body";
-  
-    var card = document.createElement("div");
-    card.className = "p-2";
-
-    var image = document.createElement("img");
-    image.setAttribute("height", "matchparent");
-    image.setAttribute("width", "100%");
-    image.src =
-      news.urlToImage ||
-      "https://i-invdn-com.investing.com/news/world_news_2_69x52._800x533_L_1419494365.jpg";
-
-    var cardBody = document.createElement("div");
-
-    var newsHeading = document.createElement("h5");
-    newsHeading.className = "card-title";
-    newsHeading.innerHTML = news.title;
-
-    var dateHeading = document.createElement("h6");
-    dateHeading.className = "text-primary";
-    dateHeading.innerHTML = date[0];
-
-    var discription = document.createElement("p");
-    discription.className = "text-muted";
-    discription.innerHTML = news.description;
-
-    var link = document.createElement("a");
-    link.className = "btn btn-dark";
-    link.setAttribute("target", "_blank");
-    link.href = news.url;
-    link.innerHTML = "Read more";
-
-    cardBody.appendChild(newsHeading);
-    cardBody.appendChild(dateHeading);
-    cardBody.appendChild(discription);
-    cardBody.appendChild(link);
-
-    card.appendChild(image);
-    card.appendChild(cardBody);
-
-    col.appendChild(card);
-
-    newsdetails.appendChild(col);
-  });
-}
